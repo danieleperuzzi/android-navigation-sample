0b24daac44d8522c4d6ad8c63738d0c5cd4f6eea
daniele.peruzzi <d.peruzzi@wakala.it>
2018-06-30 17:17:03 +0200

    sample navigation with two fragments

---
+++ app/src/main/java/com/danieleperuzzi/navigation/fragment/FirstFragment.java
@@ -0,0 +1,32 @@
+package com.danieleperuzzi.navigation.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.danieleperuzzi.navigation.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.first_fragment, container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        final Button button = view.findViewById(R.id.firstFragmentButton);
+
+        button.setOnClickListener(buttonView -> {
+            NavHostFragment.findNavController(this).navigate(R.id.action_firstFragment_to_secondFragment);
+        });
+    }
+}
---
+++ app/src/main/java/com/danieleperuzzi/navigation/fragment/SecondFragment.java
@@ -0,0 +1,21 @@
+package com.danieleperuzzi.navigation.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.danieleperuzzi.navigation.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.second_fragment, container,false);
+    }
+}
